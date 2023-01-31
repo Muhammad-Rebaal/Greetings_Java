7a474a1d5d34a887cc19dc5d726ac59a5c0c0cae
Muhammad-Rebaal <122050424+Muhammad-Rebaal@users.noreply.github.com>
2023-01-31 13:40:22 -0800

    Add files via upload

---
+++ Greetings.java
@@ -0,0 +1,12 @@
+package greetings;
+
+public class Greetings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Greetings Java Developer M.Rebaal");
+		double d = 1.1;
+
+	}
+
+}
